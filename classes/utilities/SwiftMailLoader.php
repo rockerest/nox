6eb75c536b42ceb5eba53f6c430dc6acfe272dfe
rockerest <rockerest@windows>
2012-07-09 23:38:20 -0500

    ~ significantly improved the autoloader ~ autoloader uses spl_autoload_register instead of __autoload ~ autoloader checks for a file before it includes it. LOLZ.
    ~ SwiftMailLoader actually works.
    + SML includes mail transport for all three built in transport types
    + Swiftmailer autoloader plays nice with nox autoloader thanks to spl_autoload_register
    + namespaced
    ~ currently just wraps basic message creation/sending, will be re-evaluated as more features are needed.

--- classes/utilities/SwiftMailLoader.php
+++ classes/utilities/SwiftMailLoader.php
@@ -1,28 +1,41 @@
 <?php
-	$home = implode( DIRECTORY_SEPARATOR, array_slice( explode(DIRECTORY_SEPARATOR, $_SERVER["SCRIPT_FILENAME"]), 0, -3 ) ) . '/';
-	//Get Swiftmailer
-	require_once( $home . 'lib/Swiftmailer/lib/swift_required.php');
+	namespace utilities;
 
-	class Mail{
+	//Get Swiftmailer
+	require( APPLICATION_ROOT_PATH . 'vendors/Swiftmailer/lib/swift_required.php');
+
+	class SwiftMailLoader extends \backbone\Config{
 		private $transport;
 		private $mailer;
 
-		public function __construct()
+		public function __construct( $transport = 'Mail' )
 		{
-			parent::__construct( '/apps/vmdb/', 'vmdb.conf' );
+			parent::__construct( APPLICATION_CONFIGURATION_FILE, APPLICATION_ROOT_URL );
 
-			$this->transport	= Swift_SmtpTransport::newInstance( $this->config['smtp']['host'], 25)
-										->setUsername( $this->config['smtp']['user'] )
-										->setPassword( $this->config['smtp']['pass'] );
+			switch( $transport ){
+				case 'Mail':
+					$this->transport	= \Swift_MailTransport::newInstance();
+					break;
 
-			$this->mailer		= Swift_Mailer::newInstance( $transport );
+				case 'Smtp':
+					$this->transport	= \Swift_SmtpTransport::newInstance( $this->config['smtp']['host'], $this->config['smtp']['port'] )
+											->setUsername( $this->config['smtp']['user'] )
+											->setPassword( $this->config['smtp']['pass'] );
+					break;
+
+				case 'Sendmail':
+					$this->transport	= \Swift_SendmailTransport::newInstance( $this->config['sendmail']['path'] );
+					break;
+			}
+
+			$this->mailer		= \Swift_Mailer::newInstance( $this->transport );
 		}
 
 		public function newMessage( $subject = null, $body = null, $contentType = null, $charset = null ){
-			return Swift_Message::newInstance( $subject, $body, $contentType, $charset );
+			return \Swift_Message::newInstance( $subject, $body, $contentType, $charset );
 		}
 
-		public function sendMessage( Swift_Message $message ){
+		public function sendMessage( \Swift_Message $message ){
 			return $this->mailer->send( $message );
 		}
 	}
--- components/system/Preload.php
+++ components/system/Preload.php
@@ -1,19 +1,29 @@
 <?php
 	define( 'APPLICATION_ROOT_URL', '/' );
 	define( 'APPLICATION_CONFIGURATION_FILE', 'nox.conf' );
-	define( 'APPLICATION_ROOT_PATH', implode( DIRECTORY_SEPARATOR, array_slice( explode(DIRECTORY_SEPARATOR, __FILE__), 0, -3 )) . '/');
-	define( 'APPLICATION_INCLUDE_PATH', get_include_path() . PATH_SEPARATOR . implode( PATH_SEPARATOR, array('.', 'components', 'content', 'global', 'scripts', 'images', 'styles') ) );
+	define( 'APPLICATION_ROOT_PATH', implode( DIRECTORY_SEPARATOR, array_slice( explode(DIRECTORY_SEPARATOR, __FILE__), 0, -3 ) ) . '/' ); // "-3" is number of levels to go back to root.  "/components/system/Preload.php" in this case.
+	define( 'APPLICATION_INCLUDE_PATH', get_include_path() . PATH_SEPARATOR . implode( PATH_SEPARATOR, array('.', 'components', 'content', 'global', 'scripts', 'images', 'styles' ) ) );
 
 	set_include_path( APPLICATION_INCLUDE_PATH );
 
-	function __autoload( $path ){
+	function nox_autoloader( $path ){
 		// Do a little cleaning
 		// swap back/forward slashes
 		$slash = str_replace( '\\', '/', $path );
 
-		$filename = APPLICATION_ROOT_PATH . 'classes/' . $slash . '.php';
-		require_once( $filename );
+		$locations = array(
+			'classes/'
+		);
+
+		foreach( $locations as $u ){
+			$loc = APPLICATION_ROOT_PATH . $u . $slash . '.php';
+			if( file_exists( $loc ) ){
+				require( $loc );
+			}
+		}
 	}
+
+	spl_autoload_register('nox_autoloader');
 
 	$session = new \backbone\Session(0,APPLICATION_ROOT_URL);
 	$session->setSession();
