3b403f43dd31406a1013f74b5ce940ffed14ae3a
rockerest <rockerest@windows>
2012-08-23 22:56:15 -0500

    ~ renamed /scripts/ to /js/

--- components/system/Preload.php
+++ components/system/Preload.php
@@ -2,7 +2,7 @@
 	define( 'APPLICATION_ROOT_URL', '/' );
 	define( 'APPLICATION_CONFIGURATION_FILE', 'nox.conf' );
 	define( 'APPLICATION_ROOT_PATH', implode( DIRECTORY_SEPARATOR, array_slice( explode(DIRECTORY_SEPARATOR, __FILE__), 0, -3 ) ) . '/' ); // "-3" is number of levels to go back to root.  "/components/system/Preload.php" in this case.
-	define( 'APPLICATION_INCLUDE_PATH', get_include_path() . PATH_SEPARATOR . implode( PATH_SEPARATOR, array('.', 'components', 'content', 'global', 'scripts', 'images', 'styles' ) ) );
+	define( 'APPLICATION_INCLUDE_PATH', get_include_path() . PATH_SEPARATOR . implode( PATH_SEPARATOR, array('.', 'components', 'content', 'global', 'js', 'images', 'styles' ) ) );
 
 	set_include_path( APPLICATION_INCLUDE_PATH );
 
