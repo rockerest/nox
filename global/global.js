707eb073df4998fcba834246d1b75154226fd852
rockerest <rockerest@windows>
2012-10-23 16:34:18 -0500

    + added global.js function to recursively merge objects + added global.js Cookies object to handle cookie management + added global.js Cookie object to represent a cookie

--- global/global.js
+++ global/global.js
@@ -33,7 +33,7 @@ function insertParam(key, value, retain)
     }
 	else
 	{
-		var i=kvp.length; var x; while(i--) 
+		var i=kvp.length; var x; while(i--)
 		{
 			x = kvp[i].split('=');
 
@@ -51,3 +51,103 @@ function insertParam(key, value, retain)
 		document.location.search = kvp.join('&');
 	}
 }
+
+function recursiveObjectMerge( primary, overwrite ){
+	for( var p in overwrite ){
+		try{
+			//try an update
+			if( overwrite[p].constructor == Object ){
+				primary[p] = recursiveObjectMerge( primary[p], overwrite[p] );
+			}
+			else{
+				primary[p] = overwrite[p];
+			}
+		}
+		catch( e ){
+			// destination doesn't have that property, create and set it
+			primary[p] = overwrite[p];
+		}
+	}
+
+	// primary is modified (it's a reference), but pass it back
+	// to keep up the idea that this function returns a result
+	return primary;
+}
+
+function Cookies(){
+	this.cookies = document.cookie,
+
+	this.add = function( cookie ){
+		// takes a Cookie object and adds it to the list
+		if( this.get( cookie.name ) ){
+			this.delete( cookie.name );
+		}
+
+		document.cookie = cookie.toString();
+	},
+
+	this.get = function( name ){
+		var results = this.cookies.match( '(^|;) ?' + name + '=([^;]*)(;|$)' );
+
+		if( results ){
+			return unescape( results[2] );
+		}
+		else{
+			return null;
+		}
+	},
+
+	this.delete = function( name ){
+		var past = new Date();
+		past.setTime( past.getTime() - 10101010 );
+
+		document.cookie = name += "=; expires=" + past.toGMTString();
+	}
+}
+
+function Cookie( name, value, settings ){
+	this.defaults = {
+		expire:		0,		// defaults to session
+		path:		"/",	// defaults to the root of the web server
+		domain:		null,	// no default domain
+		secure:		false	// defaults to not secure
+	};
+
+	if( typeof settings == "undefined" || !( settings instanceof Object ) ){
+		settings = {};
+	}
+
+	this.settings = recursiveObjectMerge( this.defaults, settings );
+
+	this.name = name;
+	this.value = value;
+
+	this.toString = function(){
+
+		var selfString = this.name + "=" + escape( this.value );
+
+		// set the expiration
+		if( this.settings.expire != 0 && this.settings.expire instanceof Date ){
+			selfString += "; expires=" + this.settings.expire.toGMTString();
+		}
+
+		// set the path
+		if( this.settings.path != null && this.settings.path != "" ){
+			selfString += "; path=" + escape( this.settings.path );
+		}
+
+		// set the domain
+		if( this.settings.domain != null && this.settings.domain != "" ){
+			selfString += "; domain=" + escape( this.settings.domain );
+		}
+
+		// set whether secure or not
+		if( this.settings.secure ){
+			selfString += "; secure";
+		}
+
+		return selfString;
+	}
+}
+
+
