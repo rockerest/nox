a088e9e8e6165d1f6a0a112178fac8ec8fdb6778
rockerest <rockerest@windows>
2012-10-23 16:36:27 -0500

    ~ just global.js bracket formatting

--- global/global.js
+++ global/global.js
@@ -1,51 +1,47 @@
-function go_here(loc)
-{
+function go_here(loc){
 	window.location=loc;
 	return false;
 }
 
-function getUrlVars()
-{
+function getUrlVars(){
 	var map = {};
 /* <![CDATA[ */
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
 /* ]]> */
-		function(m,key,value)
-		{
+		function(m,key,value){
       		map[key] = value;
       	}
 	);
     return map;
 }
 
-function insertParam(key, value, retain)
-{
+function insertParam(key, value, retain){
     key = escape(key); value = escape(value);
 
     var kvp = document.location.search.substr(1).split('&');
-	if( retain != null && retain == false )
-	{
+	if( retain != null && retain == false ){
 		kvp = "";
 	}
-	if (kvp == "")
-	{
+
+	if (kvp == ""){
 		document.location.search = key + "=" + value;
     }
-	else
-	{
-		var i=kvp.length; var x; while(i--)
-		{
+	else{
+		var i = kvp.length,
+			x;
+		while( i-- ){
 			x = kvp[i].split('=');
 
-			if (x[0]==key)
-			{
+			if (x[0]==key){
 					x[1] = value;
 					kvp[i] = x.join('=');
 					break;
 			}
 		}
 
-		if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+		if( i < 0 ){
+			kvp[kvp.length] = [key,value].join('=');
+		}
 
 		//this will reload the page, it's likely better to store this until finished
 		document.location.search = kvp.join('&');
